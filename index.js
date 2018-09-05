d609095734b4399505ee20961ec0171b32805498
github username <github email address>
2018-09-05 16:55:24 -0400

    Done.

--- index.js
+++ index.js
@@ -4,4 +4,58 @@ const questions = [
   {questionText: "Goldfish only have a memory of about three seconds", answer: false}
 ]
 
+
+//--------------------------------------------------------------
+
+
 let question;
+function askOneQuestion(){
+  return questions[0]
+}
+
+function displayQuestionOnClick(){
+  let btn = document.querySelector('a')
+  return btn.addEventListener('click', () => {
+    toggleTrueAndFalseButtons()
+    askQuestionThenRemoveQuestion(5000)
+  })
+}
+
+function trueAndFalseButtons(){
+  return btns = document.querySelector('.true-false-list').querySelectorAll('.btn')
+}
+
+function toggleTrueAndFalseButtons(){
+  trueAndFalseButtons().forEach(function(btn){
+    btn.classList.toggle("hide")
+  })
+}
+
+// I kind of understand
+function askQuestionThen(time){
+  question = askOneQuestion();
+  appendQuestion(question)
+  return new Promise((resolve) => {
+    setTimeout(function(){
+      resolve(question)
+    }, time)
+  })
+}
+
+// I understand
+function appendQuestion(question){
+  let container = document.querySelector('.question-container')
+  container.innerHTML = question.questionText;
+}
+
+function removeQuestion(){
+  return new Promise(function(){
+    let container = document.querySelector('.question-container')
+    container.innerHTML = ''
+    toggleTrueAndFalseButtons()
+  })
+}
+
+function askQuestionThenRemoveQuestion(time){
+  return askQuestionThen(time).then(removeQuestion)
+}
